1961d5fd65e1f867c79c99374ef680e2767f1879
bonuelc <bonuelc@gmail.com>
2016-12-06 15:57:40 -0700

    Add SplitViewController.swift

---
+++ DiaryApp/SplitViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplitViewController.swift
+//  DiaryApp
+//
+//  Created by Christopher Bonuel on 12/6/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
