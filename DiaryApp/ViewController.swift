f8a9710e213195a40c08fa9d9f2cfc7e17458862
bonuelc <bonuelc@gmail.com>
2016-12-06 15:26:34 -0700

    Initial Commit

---
+++ DiaryApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by Christopher Bonuel on 12/6/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
