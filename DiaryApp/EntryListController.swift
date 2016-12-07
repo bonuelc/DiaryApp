840366a9c8574d8f836bc00e678801c97de25f5a
bonuelc <bonuelc@gmail.com>
2016-12-07 13:55:52 -0700

    Add full-screen tableView to EntryListController

--- DiaryApp/EntryListController.swift
+++ DiaryApp/EntryListController.swift
@@ -11,12 +11,33 @@ import CoreData
 
 class EntryListController: UIViewController {
     
+    // MARK: - UI
+    
+    lazy var tableView: UITableView = {
+        return UITableView()
+    }()
+    
     // MARK: Properties
     
     var managedObjectContext: NSManagedObjectContext?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Layout
+    
+    override func viewWillLayoutSubviews() {
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activateConstraints([
+            tableView.leftAnchor.constraintEqualToAnchor(view.leftAnchor),
+            tableView.topAnchor.constraintEqualToAnchor(topLayoutGuide.topAnchor),
+            tableView.rightAnchor.constraintEqualToAnchor(view.rightAnchor),
+            tableView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor),
+        ])
     }
 }
 
