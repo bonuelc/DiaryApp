dde4a0db1daa85621f0286b393ca84b886475b7b
bonuelc <bonuelc@gmail.com>
2016-12-07 13:27:51 -0700

    Add EntryViewController
    # Conflicts:
    #	DiaryApp.xcodeproj/project.pbxproj

---
+++ DiaryApp/EntryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EntryViewController.swift
+//  DiaryApp
+//
+//  Created by Christopher Bonuel on 12/6/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
