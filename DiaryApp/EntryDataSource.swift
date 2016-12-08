53602f1f98241abb94c7c40acbad5d9f6e10ccda
bonuelc <bonuelc@gmail.com>
2016-12-08 11:48:10 -0700

    Implement EntryDataSource
    TODO: implement, register, and reuse a custom UITableViewCell

---
+++ DiaryApp/EntryDataSource.swift
@@ -0,0 +1,48 @@
+//
+//  EntryDataSource.swift
+//  DiaryApp
+//
+//  Created by Christopher Bonuel on 12/8/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class EntryDataSource: NSObject, UITableViewDataSource {
+    
+    let tableView: UITableView
+    let fetchedResultsController: EntryFetchedResultsController
+    
+    init(tableView: UITableView, fetchRequest: NSFetchRequest, managedObjectContext: NSManagedObjectContext) {
+        
+        self.tableView = tableView
+        self.fetchedResultsController = EntryFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: managedObjectContext)
+        
+        super.init()
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        guard let section = fetchedResultsController.sections?[section] else { return 0 }
+        
+        return section.numberOfObjects
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        let entry = fetchedResultsController.objectAtIndexPath(indexPath) as! Entry
+        
+        cell.textLabel!.text = entry.text
+        
+        return cell
+    }
+}
