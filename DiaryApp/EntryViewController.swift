a4ea187125e57b3e2d7b4e95d447386b2de9913f
bonuelc <bonuelc@gmail.com>
2016-12-08 12:40:47 -0700

    Add saveBarButtonItem

--- DiaryApp/EntryViewController.swift
+++ DiaryApp/EntryViewController.swift
@@ -76,6 +76,9 @@ extension EntryViewController {
         
         let cancelBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: #selector(cancelBarButtonItemTapped))
         navigationItem.leftBarButtonItem = cancelBarButtonItem
+        
+        let saveBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Save, target: self, action: #selector(saveBarButtonItemTapped))
+        navigationItem.rightBarButtonItem = saveBarButtonItem
     }
     
     @objc func cancelBarButtonItemTapped() {
