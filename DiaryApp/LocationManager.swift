3320f95858b132107ae6aa39e0da3f4bfdf62cad
bonuelc <bonuelc@gmail.com>
2016-12-11 09:01:35 -0700

    Add LocationManager class

---
+++ DiaryApp/LocationManager.swift
@@ -0,0 +1,12 @@
+//
+//  LocationManager.swift
+//  DiaryApp
+//
+//  Created by Christopher Bonuel on 12/11/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import CoreLocation
+
+class LocationManager: CLLocationManager {
+}
