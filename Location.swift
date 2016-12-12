2835b6150ea491e001f85f9acd1f101504ce6d8c
bonuelc <bonuelc@gmail.com>
2016-12-12 11:13:46 -0700

    Implement Location factory method

--- Location.swift
+++ Location.swift
@@ -8,9 +8,26 @@
 
 import Foundation
 import CoreData
+import CoreLocation
 
 
 class Location: NSManagedObject {
     
     static let entityName = "\(Location.self)"
+    
+    class func location(withPlacemark placemark: CLPlacemark?, inManagedObjectContext moc: NSManagedObjectContext) -> Location? {
+        
+        guard let placemark = placemark, cllocation = placemark.location else { return nil }
+        
+        let location = NSEntityDescription.insertNewObjectForEntityForName(entityName, inManagedObjectContext: moc) as! Location
+        
+        location.latitude = cllocation.coordinate.latitude
+        location.longitude = cllocation.coordinate.longitude
+        
+        location.name = placemark.name
+        location.city = placemark.locality
+        location.area = placemark.administrativeArea
+        
+        return location
+    }
 }
