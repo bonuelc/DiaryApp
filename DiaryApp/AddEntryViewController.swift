693f03019ca6f38292a31c40b71409de065a773f
bonuelc <bonuelc@gmail.com>
2017-03-30 13:23:28 -0600

    Add AddEntryVC
    This VC will simply have an 'Add' button to replace the fake EntryVC on
    launch

---
+++ DiaryApp/AddEntryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddEntryViewController.swift
+//  DiaryApp
+//
+//  Created by Christopher Bonuel on 3/30/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class AddEntryViewController: UIViewController {
+}
